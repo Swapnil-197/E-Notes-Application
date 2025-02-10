fcbb093fca4c6c80eecd6bfdee74d03203b3e644
Swapnil-197 <sspatil1907@gmail.com>
2025-02-10 18:01:58 +0530

    DTO & Mapper concept

--- src/main/java/com/enote/dto/CategoryResponse.java
+++ src/main/java/com/enote/dto/CategoryResponse.java
@@ -1,23 +1,11 @@
-package com.enote.entity;
+package com.enote.dto;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+public class CategoryResponse {
 
-@Entity
-public class Category extends BaseModel {
-
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String name;
 	private String description;
 
-	public Category() {
-		super();
-
-	}
 
 	public Integer getId() {
 		return id;
--- src/main/java/com/enote/service/CategoryService.java
+++ src/main/java/com/enote/service/CategoryService.java
@@ -4,11 +4,14 @@ import java.util.List;
 
 import org.springframework.stereotype.Service;
 
+import com.enote.dto.CategoryDto;
+import com.enote.dto.CategoryResponse;
 import com.enote.entity.Category;
 
 @Service
 public interface CategoryService {
 
-	public Boolean saveCategory(Category category);
-	public List<Category> getAllCategory();
+	public List<CategoryDto> getAllCategory();
+	public Boolean saveCategory(CategoryDto categoryDto);
+	public List<CategoryResponse> getActiveCategory();
  }
